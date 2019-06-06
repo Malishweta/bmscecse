c0533bf97a03fb7f869fa25d6bed310aa401f95c
Malishweta <51438131+Malishweta@users.noreply.github.com>
2019-06-06 14:31:22 +0530

    Update add.c

--- add.c
+++ add.c
@@ -1,1 +1,51 @@
 
+#include<stdio.h>
+int main()
+{
+	int ch;
+	printf("Enter your choice:\n");
+	scanf("%d\n",ch);
+	for(;;)
+	switch(ch)
+	{
+		case 1:add1();
+			break;
+		case 2:add2();
+			break;
+		case 3:exit(0);
+		default:printf("choice correct option!!!\n");
+	}
+
+}
+int add1()
+{
+	int a,b,c;
+	printf("Enter a and b values:\n");
+	scanf("%d%d",&a,&b);
+	if(a>=5 && b<=10)
+	{
+		return c=a+b;
+	}
+	else
+	{
+		printf("Try again\n");
+	}
+	printf("SUM:\n",c);
+	return 0;
+}
+int add2()
+{
+	int a1,b1,c1;
+	printf("Enter a1 and b1 values:\n");
+	scanf("%d%d",a1,b1);
+	if(a1>=20 && b1<=50)
+	{
+		return c1=a1+b1;
+	}
+	else
+	{
+		printf("Try again!!!\n");
+	}
+	printf("SUM:\n",c1);
+	return 0;
+}
