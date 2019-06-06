f9e7805c00d9f40f0108ef78d71bacd4f2233955
Malishweta <51438131+Malishweta@users.noreply.github.com>
2019-06-06 15:57:31 +0530

    Update add.c

--- add.c
+++ add.c
@@ -1,58 +1,22 @@
 #include<stdio.h>
-#include<stdlib.h>
-void add1();
-void add2();
+void getinput(int *a,int *b)
+{
+	printf("enter a and b values:\n");
+	scanf("%d%d",a,b);
+}
+int add(int a,int b)
+{
+	int c;
+	return c=a+b;
+}
+void getoutput(int a,int b,int c)
+{
+	printf("%d+%d=%d\n",a,b,c );
+}
 int main()
 {
-	int ch;
-	printf("1.add1\n2.add2\n3.Exit\n");
-	printf("Enter your choice:\n");
-	scanf("%d\n",&ch);
-	
-	switch(ch)
-	{
-		case 1:add1();
-			break;
-		case 2:add2();
-			break;
-		case 3:exit(0);
-		default:printf("choose correct option!!!\n");
-	}
-
-}
-void add1()
-{
-	int a,b,c;
-	printf("Enter a and b  value:\n");
-	scanf("%d%d",&a,&b);
-	
-	if(a>=10)
-	{
-		 c=a+b;
-		printf("SUM:%d\n",c);
-
-	}
-	else
-	{
-		printf("Try again\n");
-	}
-	
-}
-void add2()
-{
-	int a1,b1,c1;
-	printf("Enter a1 and b1 value:\n");
-	scanf("%d%d",&a1,&b1);
-	
-	if(a1>=20)
-	{
-		c1=a1+b1*2;
-		printf("SUM:%d\n",c1);
-	}
-	else
-	{
-		printf("Try again!!!\n");
-	}
-	
-	
+	int x,y,z;
+	getinput(&x,&y);
+	z=add(x,y);
+	getoutput(x,y,z);
 }
